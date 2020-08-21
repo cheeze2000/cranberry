7dc7b5bca614691628d4e232368802f8354bd31b
Nicholas <me@cheeze2000.net>
2020-08-21 23:35:01 +0800

    feat(): rewrite complete

---
+++ src/Cranberry/Payload.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Cranberry.Payload where
+
+import Data.Aeson
+import Data.Aeson.TH
+import Text.Casing (fromHumps, toQuietSnake)
+
+heartbeatPayload :: Int -> Value
+heartbeatPayload seq = object
+  [ "op" .= (1 :: Int)
+  , "d"  .= (seq :: Int)
+  ]
+
+identifyPayload :: String -> Value
+identifyPayload token = object
+  [ "op" .= (2 :: Int)
+  , "d"  .= object
+    [ "token" .= token
+    , "properties" .= object
+      [ "$os"      .= ("Cranberry" :: String)
+      , "$browser" .= ("Cranberry" :: String)
+      , "$device"  .= ("Cranberry" :: String)
+      ]
+    ]
+  ]
+
+data Payload = Payload
+  { op :: Int
+  , d  :: Maybe Value
+  , s  :: Maybe Int
+  , t  :: Maybe String
+  }
+
+data MessageCreatePayload = MessageCreatePayload
+  { channelId :: String
+  , content   :: String
+  }
+
+deriveJSON defaultOptions ''Payload
+deriveJSON defaultOptions{fieldLabelModifier = toQuietSnake . fromHumps} ''MessageCreatePayload
---
+++ src/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Cranberry.WebSocket (client)
+
+main :: IO ()
+main = client
