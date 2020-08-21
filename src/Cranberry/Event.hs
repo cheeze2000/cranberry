09cd224cc01e6745a90e166c7f1f442eb4644329
Nicholas <me@cheeze2000.net>
2020-08-22 00:27:04 +0800

    refactor(Event): add command handler and default prefix

--- src/Cranberry/Event.hs
+++ src/Cranberry/Event.hs
@@ -5,18 +5,27 @@ import Cranberry.Request
 
 import Data.Aeson (Value, decode, encode)
 import Data.ByteString.Lazy (ByteString)
+import Data.List (isPrefixOf)
 import Data.Maybe (fromJust)
 
+type Command = String
 type EventData = ByteString
 type EventType = String
 
+prefix :: String
+prefix = "c."
+
 onMessageCreate :: Token -> EventData -> IO ()
 onMessageCreate token d = do
-  let msg = fromJust $ decode d :: MessageCreatePayload
-  let c = channelId msg
-  if content msg == "ping"
-    then createMessage token c "pong!"
+  let msg = fromJust $ decode d :: Message
+  let c = content msg
+  if prefix `isPrefixOf` c
+    then handleCommand token msg . drop 2 $ c
     else return ()
+
+handleCommand :: Token -> Message -> Command -> IO ()
+handleCommand t m "ping" = createMessage t (channelId m) "pong!"
+handleCommand t m _ = return ()
 
 handleEvent :: Token -> EventType -> EventData -> IO ()
 handleEvent token t d = do
