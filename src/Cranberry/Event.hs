12951f6e40017d4f0385db671833b7204b3bc513
Nicholas <me@cheeze2000.net>
2020-08-25 23:23:11 +0800

    feat(Event): improve command handler and add emoji command

--- src/Cranberry/Event.hs
+++ src/Cranberry/Event.hs
@@ -4,6 +4,7 @@ import Cranberry.Payload
 import Cranberry.Request
 
 import Data.Aeson (Value, decode, encode)
+import Data.Char (isDigit)
 import Data.ByteString.Lazy (ByteString)
 import Data.List (isPrefixOf)
 import Data.Maybe (fromJust)
@@ -11,6 +12,10 @@ import Data.Maybe (fromJust)
 type Command = String
 type EventData = ByteString
 type EventType = String
+
+parse :: String -> (Command, [String])
+parse xs = (y, ys)
+  where (y:ys) = words xs
 
 prefix :: String
 prefix = "c."
@@ -20,11 +25,15 @@ onMessageCreate token d = do
   let msg = fromJust $ decode d :: Message
   let c = content msg
   if prefix `isPrefixOf` c
-    then handleCommand token msg . drop 2 $ c
+    then handleCommand token msg . parse . drop 2 $ c
     else return ()
 
-handleCommand :: Token -> Message -> Command -> IO ()
-handleCommand t m "ping" = createMessage t (channelId m) "pong!"
+handleCommand :: Token -> Message -> (Command, [String]) -> IO ()
+handleCommand t m ("emoji", xs) = createMessage t (channelId m) url
+  where url = "https://cdn.discordapp.com/emojis/"
+           ++ takeWhile isDigit (dropWhile (not . isDigit) $ unwords xs)
+           ++ ".png?size=1024"
+handleCommand t m ("ping", _) = createMessage t (channelId m) "pong!"
 handleCommand t m _ = return ()
 
 handleEvent :: Token -> EventType -> EventData -> IO ()
